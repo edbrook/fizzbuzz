9f50583e39ce7a56fab17b9bd4d3737bfe515ae5
Ed Brook <edbrook@gmail.com>
2022-02-20 14:20:46 +0000

    cleanup / naming / linebreaks

--- Replacement.java
+++ Replacement.java
@@ -7,16 +7,16 @@ public class Replacement<T,R> {
     private final Function<T, R> mapper;
     private final Predicate<T> predicate;
 
-    public static <T,R> Replacement<T,R> of(R mapper, Predicate<T> predicate) {
-        return new Replacement<T,R>(n -> mapper, predicate);
+    public static <T,R> Replacement<T,R> of(R constantValue, Predicate<T> predicate) {
+        return new Replacement<>(n -> constantValue, predicate);
     }
-    
-    public static <T,R> Replacement<T,R> of(Supplier<R> mapper, Predicate<T> predicate) {
-        return new Replacement<T,R>(n -> mapper.get(), predicate);
+
+    public static <T,R> Replacement<T,R> of(Supplier<R> valueSupplier, Predicate<T> predicate) {
+        return new Replacement<>(n -> valueSupplier.get(), predicate);
     }
-    
+
     public static <T,R> Replacement<T,R> of(Function<T, R> mapper, Predicate<T> predicate) {
-        return new Replacement<T,R>(mapper, predicate);
+        return new Replacement<>(mapper, predicate);
     }
 
     public Optional<R> replace(T n) {
